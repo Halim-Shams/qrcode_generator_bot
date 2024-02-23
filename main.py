74313938855bd58e04a7fe5c94cdb743b402f045
Halim <halimshams8@gmail.com>
2024-02-23 18:07:37 +0430

    updated

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ load_dotenv()
 API_TOKEN = os.getenv("API_TOKEN")
 BOT_USERNAME = os.getenv("BOT_USERNAME")
 
+print(API_TOKEN)
+print(BOT_USERNAME)
+
 # Qrcode generator
 def qrcode_generator(text: str):
     qrcode_img = qrcode.make(text)
